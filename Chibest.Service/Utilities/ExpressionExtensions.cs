8dfcfdf5c3bad04984df311ecd8bd54088180d06
Johnny Pham <johnnypham14112003@gmail.com>
2025-10-22 13:57:39 +0700

    Refactor role definitions and expression extensions
    Moved system role definitions from RoleEnum to Const.Roles and deleted the RoleEnum file. Refactored Const.cs to use static class and constants for roles. Simplified ExpressionExtensions.And method by removing ParameterReplacer and using Expression.Invoke for combining expressions. Added Chibest.Common project reference to Chibest.API.

--- Chibest.Service/Utilities/ExpressionExtensions.cs
+++ Chibest.Service/Utilities/ExpressionExtensions.cs
@@ -4,24 +4,13 @@ namespace Chibest.Service.Utilities;
 
 public static class ExpressionExtensions
 {
-    public static Expression<Func<T, bool>> And<T>(
-        this Expression<Func<T, bool>> left,
-        Expression<Func<T, bool>> right)
+    public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
     {
-        if (left == null) return right;
-        if (right == null) return left;
-
         var parameter = Expression.Parameter(typeof(T));
-        var leftBody = new ParameterReplacer(parameter).Visit(left.Body);
-        var rightBody = new ParameterReplacer(parameter).Visit(right.Body);
-        var body = Expression.AndAlso(leftBody!, rightBody!);
+        var body = Expression.AndAlso(
+            Expression.Invoke(expr1, parameter),
+            Expression.Invoke(expr2, parameter)
+        );
         return Expression.Lambda<Func<T, bool>>(body, parameter);
-    }
-
-    private class ParameterReplacer : ExpressionVisitor
-    {
-        private readonly ParameterExpression _parameter;
-        public ParameterReplacer(ParameterExpression parameter) => _parameter = parameter;
-        protected override Expression VisitParameter(ParameterExpression node) => _parameter;
     }
 }
