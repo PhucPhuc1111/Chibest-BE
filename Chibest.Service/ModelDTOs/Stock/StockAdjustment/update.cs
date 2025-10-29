5553f657555e7648e9cf29ed6a841d2c7b931f41
Le Nguyen Hoang Phuc <133560614+PhucPhuc1111@users.noreply.github.com>
2025-10-29 10:05:58 +0700

    update stockAdjustment

---
+++ Chibest.Service/ModelDTOs/Stock/StockAdjustment/update.cs
@@ -0,0 +1,32 @@
+﻿using Chibest.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chibest.Service.ModelDTOs.Stock.StockAdjustment
+{
+    public class update
+    {
+        public class StockAdjustmentUpdate
+        {
+            public AdjustmentType? AdjustmentType { get; set; }
+            public Guid? ApprovebyId { get; set; }
+            public string? Note { get; set; }
+            public string? Status { get; set; }
+            public List<StockAdjustmentDetailUpdate> StockAdjustmentDetails { get; set; } = new();
+        }
+
+        public class StockAdjustmentDetailUpdate
+        {
+            public Guid Id { get; set; }
+            public Guid ProductId { get; set; }
+            public int SystemQty { get; set; }
+            public int ActualQty { get; set; }
+            public decimal UnitCost { get; set; }
+            public string? Reason { get; set; }
+            public string? Note { get; set; }
+        }
+    }
+}
