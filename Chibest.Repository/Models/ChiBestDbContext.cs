5b1b443354d1cbd02439a883601d4a1b0eae82f5
Johnny Pham <johnnypham14112003@gmail.com>
2025-10-30 11:47:41 +0700

    fix: hardcode connectionString

--- Chibest.Repository/Models/ChiBestDbContext.cs
+++ Chibest.Repository/Models/ChiBestDbContext.cs
@@ -83,10 +83,6 @@ public partial class ChiBestDbContext : DbContext
 
     public virtual DbSet<WorkShift> WorkShifts { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Database=ChiBestDB;Username=postgres;Password=Password14112003;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Account>(entity =>
