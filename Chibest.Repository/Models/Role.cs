2c13ecdd52a80304806f04977b23a5495791770f
Le Nguyen Hoang Phuc <133560614+PhucPhuc1111@users.noreply.github.com>
2025-11-17 10:59:31 +0700

    add permission

--- Chibest.Repository/Models/Role.cs
+++ Chibest.Repository/Models/Role.cs
@@ -10,4 +10,6 @@ public partial class Role
     public string Name { get; set; } = null!;
 
     public virtual ICollection<AccountRole> AccountRoles { get; set; } = new List<AccountRole>();
+
+    public virtual ICollection<Permission> Permissions { get; set; } = new List<Permission>();
 }
